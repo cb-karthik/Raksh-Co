cf445c245d17fd1371b1b08737414687f486d68e
karthik <geokarthikcb@gmail.com>
2023-10-27 10:17:24 +0530

    final

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className=" flex flex-col items-center justify-center  w-full  h-screen gap-4 ">
+      <div className="text-blue-400 font-bold text-3xl animate-bounce ">
+        Wellcome to Raksh & Co
+      </div>
+      <img src="https://i.ytimg.com/vi/DpjvXVWGVNs/maxresdefault.jpg" alt="" />
+      <div className="text-red-400 font-bold text-3xl animate-pulse">
+        Website under maintance
+      </div>
+
+      <img src="/assets/Raksh&CO.jpg" alt="" />
+    </div>
+  );
+}
+
+export default App;
